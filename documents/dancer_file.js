630fc02e87977badd30d0ccc75b3bc7c42a09910
jess-ingraham <jli232@nyu.edu>
2018-10-21 16:04:54 -0400

    dancer page added

---
+++ documents/dancer_file.js
@@ -0,0 +1,68 @@
+
+const dancers={
+	'freshman' : [
+		{name:'Ana Monfared', currentHeadshot: '', currentPose:''}
+		,{name:'Olivia Bloom', currentHeadshot: '', currentPose:''}
+		,{name:'Hannah Womack', currentHeadshot: '', currentPose:''}
+		,{name:'Jordan Lopez', currentHeadshot: '', currentPose:''}
+		,{name:'Chelsea Connell', currentHeadshot: '', currentPose:''}
+		,{name:'Keith Seguin-Gumanti', currentHeadshot: '', currentPose:''}
+		,{name:'Peyton Dillehay', currentHeadshot: '', currentPose:''}
+		]
+
+	'sophomore':[
+		{name: 'Oriana Siphanoum', currentHeadshot:'', currentPose:''}
+		,{name: 'Everett Spink', currentHeadshot:'', currentPose:''}
+		,{name: 'Mars Bernie', currentHeadshot:'https://lh5.googleusercontent.com/5pmGO2J6rIA6ZzKZWw5UHZjoBwB_1su7TiiTPkfqhm40y-yq2usJBYCuES0xAzf0hCdXZ4LXLtXEzVPLhU6j=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/ZV0qFz5q9pG9cPFgeLLy_U1I8IWRa427Em7VITOryKih7IMwZhBpKsREtGMCKth3A8u0_DYirEiyfmvFIc94=w1366-h626-rw'}
+		,{name: 'Emma Pretzer', currentHeadshot:'https://lh3.googleusercontent.com/VuzB_0veGDGlI77BTGUVB4xQGbY8yM9swvwkYdBZEbn-lQnXmidCoRE0esrnW7PWN0K-ZZa3E91LdJs7UyXY=w1366-h626-rw', currentPose:'https://lh3.googleusercontent.com/n1hyJuvYRwhrpq3U0-kebtrXloeWFJWKi4vLp8A2bWUYe98k_jvnNmGx-zXFB-hn0YB6sgZNJVVqQCAGxRZc=w1366-h626-rw'}
+		,{name: 'Mackenzie Testa', currentHeadshot:'', currentPose:''}
+		,{name: 'Tyler McNair', currentHeadshot:'https://lh4.googleusercontent.com/YKVz4kuHtg6yxEvEMJBt8GcIT-T7JL10y2JEYXI1lzfymoytMOrt5Z5_rgppQEglpTNV4k-SsuviYZJN6McR=w1366-h626-rw', currentPose:'https://lh5.googleusercontent.com/5F8iISsNGSafrEI7T9Cd-3ORHK9yORyLWs6FoLzeq_gUZtB3m8a5RRMK-IT3_ci8eODwcvdBhA0WAaboIqvg=w1366-h626-rw'}
+		,{name: 'Grace Corry', currentHeadshot:'', currentPose:''}
+		,{name:'Megan Rose', currentHeadshot: '', currentPose:''}
+		,{name:'Stephanie Hui', currentHeadshot: '', currentPose:''}
+		,{name:'Kate Day', currentHeadshot: 'https://lh5.googleusercontent.com/1qTfG8fHCGYEBV42DuwMXP4SyHSdbDJPI-Rls49-myBeZpynNYpiKDTZL6byzKEFj0eCfRRwXe-Lkg-KAmuX=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/wMROBy87uFw5tOVKr3rNk76MDpI51T-jXoKnw4z5P916dmttQ5pZAJbqjBnL6naHFpXsN1pLqoLBq6LUr78Y=w1366-h626-rw'}
+		,{name:'Julia Nicholls', currentHeadshot: '', currentPose:''}
+		,{name:'Megan Schwarz', currentHeadshot: '', currentPose:''}
+		,{name:'Jaime Ostrow', currentHeadshot: 'https://lh4.googleusercontent.com/aadD0pp2nythZ1Z8EFA9vB6ysvRr78V-Pq91d1ZBd4ok3ceWjFqmm5NoX8-2ibYzkjrJZoRqSZtkuvDPtum4=w1366-h626-rw', currentPose:'https://lh5.googleusercontent.com/wC36Fm97y2exDJqfB90F538OdfxRGkY9AkpjedIakznHGvN_ItNhj3gLU1EPrwhcwridEU2ZA9_aY1ORMqPy=w1366-h626-rw'}
+		,{name:'Justin Min', currentHeadshot: '', currentPose:''}
+		,{name:'Delaney Andersen', currentHeadshot: 'https://lh3.googleusercontent.com/trf2yOXf8NKFtEVG_EdF4F6q0EegKPKreqU4Oz-L_h9IuJ7xZSmhLtivuP4n43A0zAlXmxlyTYIBokl_dUhk=w1366-h626-rw', currentPose:'https://lh3.googleusercontent.com/pu7ycaP5Xzqe8ApqMI2hC-BswUbSsHTeNdh-kr5II6pVj4dSLQgNSz2K3hQ80rK66nDF5AvBH6ZKg8zpM9-H=w1366-h626-rw'}
+		,{name:'Jenna Szoke', currentHeadshot: 'https://lh5.googleusercontent.com/lXBgHFOc1vSnoM-byPREiP-2YmbFwS_5e2SJ1QHRReSubOLGR7xzfwcvvn3pzCslCGiQIzko_qAIIUW9zF9V=w1366-h626-rw', currentPose:'https://lh5.googleusercontent.com/OX5m6JFvql_TWXZsPLYmClUSQp2Sj5HlQidT4OSwigoWRWbLVn0j3_OfA1JlRqg4pOU7-HfX_et9VdWrHvX1=w1366-h626-rw'}
+		,{name:'Amanda DeVito', currentHeadshot: 'https://lh3.googleusercontent.com/zAn_v3DFlKd73yRnO3OGy7L6SYkIIg_sB32v0I7G3MBQav8tDcH2XddE2e9NBJ7zm7FwuSwr_xCxog30bW6S=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/EUGgG8xpfjXJVdySCWM1rrpzsehn1EffHMmsK7J6VfnW_OLK8xJNcZhfaupHxZyaQM2775KEOkCkyvqhWBMG=w1366-h626-rw'}
+		,{name:'Olympia Spivey', currentHeadshot: '', currentPose:''}
+	]
+
+	'junior': [
+		{name:'Sarina Gonzalez', currentHeadshot: '', currentPose:''}
+		,{name:'Marisa Simon', currentHeadshot: 'https://lh5.googleusercontent.com/W_I24lIjxM73Z0NnHS45y5yETJOL2aG0vwxXSSX87_UJLwJJvPIu4mpdDvdotQe17ABitWZQfYKp-HE6EskY=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/1BYVXjE0b_LTACVEKaxJWcsm-5r-hqwIsTBeC0HSeEFCBF61flFmuU1IswfZLt7NfkV96x8rD60wF73gFzCI=w1366-h626-rw'}
+		,{name:'Daniel Thimm', currentHeadshot: '', currentPose:''}
+		,{name:'Charlotte Blumenfeld', currentHeadshot: 'https://lh5.googleusercontent.com/fr25R9UpuOn0Cr5gTOH5mIX5heb4CxBigQO7_DldNbPNysik5P3TGO5NW0idxJiq5GvXfkgmzrXU91lOqtsS=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/3qpY4Wu-V2itcwcrvW_nmtvC1OjkMOyDatX7cq6_Sh3MPLodEIfJpHbl2SEzU3vlImMP9c8ECqYEaTu9_zie=w1366-h626-rw'}
+		,{name:'Quinn Marshall', currentHeadshot: 'https://lh3.googleusercontent.com/YE3AuEXPI-u-zJqAiei8xx5l_pbInxDCfsS5XqWOT51EnVcca-uFEI3XN48LsMucdJZnt1scvbbNO-ntJqsG=w1366-h626-rw', currentPose:'https://lh4.googleusercontent.com/dp9H_HRDHFIkqolszCXtBPlyGCFhZ7PlgwGpIIf9KGp8sFXpogTq9SRu7ln_uruW60_EiJKg3_3o6xKeaTr_=w1366-h626-rw'}
+		,{name:'Chelsea Galembo', currentHeadshot: '', currentPose:''}
+		,{name:'Christabelle Tan', currentHeadshot: 'https://lh4.googleusercontent.com/TKTQOIAesODuv9tNZnF_1Em3GNUHEz1l329iPO-_dxaZK_S5EP6kT37DCnVaSVCaKpVtG4w8DCepcfvPFN0y=w1366-h626-rw', currentPose:'https://lh4.googleusercontent.com/IBIJeC4j-SA4FvdxN7ECJSKCtZcjaZgoPE_lkCzYfyfSEQpsOhLlAFQVEBvvdv8_pN3tDrOjCN14cfb4abIw=w1366-h626-rw'}
+		,{name:'Christie Nakajima', currentHeadshot: 'https://lh6.googleusercontent.com/73ldY64R6yNBNP2_LOCttY2_PoSP3OeFaZTFUCfvDP947Vh9nV4AGCrBQoxk9nP4519ZGu3e02pg0tvR7Lks=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/x33Am-d9NRDQsxJoWpOmmJd6uGNCzAUwijFwAaN5iywuodYDnY9wc_sQai7mXTH_llzKfoS2UoZMIV43YkC0=w1366-h626-rw'}
+		,{name:'Hadar Landan', currentHeadshot: 'https://lh4.googleusercontent.com/T1QgzrA9R1EZXU4YeaoFHMvF2U6qmwNR_ViKRfpm3-lEOkEKz7hzbyYb28GQJe4O4Te6iKzPOpysPAZcpPEf=w1366-h626-rw', currentPose:'https://lh5.googleusercontent.com/VhFYy57oUJs-3D74TO58fpcfuNvMHilzW4JSTXS5lsLNxEE5VUbdBg1kJFojHqYCWPA7EyB2VHWIff9uvXNC=w1366-h626-rw'}
+	]
+
+	'senior': [
+		{name:'Julia Salkin', currentHeadshot: 'https://lh6.googleusercontent.com/sxkWmpMX7lDYO4r0MQK16-iQ7Y7BNDB2WYHwv4Qw80ZjxTvls6U4-ukJnt3R0LVPCboI_KeeVUKXufLlkaUn=w1366-h626-rw', currentPose:'https://lh4.googleusercontent.com/3YAkZHya-bAgpelR2KAGSNc3mWAUrvXq8HCDnIA9apDDBqA18EEmmlzcQRul2Ou0ySXn3hyT0k1Mb2FarIIP=w1366-h626-rw'}
+		,{name:'Alex Leatherwood', currentHeadshot: 'https://lh3.googleusercontent.com/7UWNBJwklinIoHLn1Uc5j_8J2KiDRfMMQLKSoaqx7r84qMP2IP2HBRPG4GUFpKnpCH7IPpQ_sNsLhy0O0LLr=w1366-h626-rw', currentPose:'https://lh3.googleusercontent.com/hvClOblwbg-QO2VfbfsoKXf5WY-e2DKx2b0fFqhp0ikhjTMr72beXqrRMT6xgwe6RmZOZyVyuPIDgQxIxiyn=w1366-h626-rw'}
+		,{name:'Jess Ingraham', currentHeadshot: 'https://lh5.googleusercontent.com/La_9jgJuctcT6T07060VBkqQGgDtXK29qhoMTvhwTqX3Ga7l_1aCW8Erbo-BgCukkb0axGHHA8TyfTaaFY1r=w1366-h626-rw', currentPose:'https://lh5.googleusercontent.com/bbpKkpZxXqkVno4MbVi2mlttzoZLG8O5-w8Ua26r7WhCYwgfWFGh9KU1mpZOOGeDBQNQHJ8qcJUSmfyWdzj1=w1366-h626-rw'}
+		,{name:'Selena Robinson', currentHeadshot: 'https://lh3.googleusercontent.com/SZR6uVu3V9QyyMZyrSUTyrWIVHhZRFl68U1wYKDYb07k2nQsZEkaTeJesM4FFdsxGSjEGE6PWOYkGkRWMd_q=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/C2YEdmrxB89leujtge24N6l-QcPRS99WgWIp1GLg7bVt7wuYz0vHAbg67UQxdUDrXWOf_n4tT7NoBh3gQwwc=w1366-h626-rw'
+			,bio: ''}
+		,{name:'Jackie Keyasko', currentHeadshot: 'https://lh3.googleusercontent.com/8Ij4lujVE2wRZtyWcP5Q-etTZsJQidILxTWhKFpAEIfieni688r--cC2r_dVaIzUwwuvOzk10jqFcmzMzaKp=w1366-h626-rw', currentPose:'https://lh4.googleusercontent.com/spCP0jqnEKH-SVE7igdW2Tt8ZC0EceTwZayKlN0rzAv5v8v03TeRn9Yf5L1H9vDYYa8tUVh4x5vOKXzVRKK1=w1366-h626-rw'}
+		,{name:'Kyle Livsey', currentHeadshot: '', currentPose:''}
+		,{name:'Benjamin Freemantle', currentHeadshot: 'https://lh5.googleusercontent.com/Mva0thOZVWxrRgMwRFvLtbLwEuzINW_6_vlNs5rkTHiR25NY67CvsoNIvVhOjV4vfm9PBoZev0D99nUYZwsk=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/a10Qs39x3_uIRll8PZ_inpmI7JvWOVrC3fmWDY6SZXNEKeeqZ8M5EXg3iJfhNhdfZhhgC6WrsuqiUgplF6o4=w1366-h626-rw'}
+	]
+
+	'alumni':[
+		{name:'KP Mendoza', currentHeadshot:'https://lh6.googleusercontent.com/T_UB8EcPPY2YOJtXIfejTYQDeLtLBw2kj7A_kVVRJz_utuOt6LQpA2YwaZCR1SIuwq4z-nADXkqRSl2UiXRM=w1366-h626-rw', currentPose:'https://lh4.googleusercontent.com/JNSOkyP0NkQkenNaqeznIgTnEDri1GAMp5iornsLfsjWSB_xNyDF5P73HibH9pOopvcb544zURzt6hYAdPk3=w1366-h626-rw'}
+		,{name:'Youran Lee', currentHeadshot:'https://lh6.googleusercontent.com/Bl7sKUTXYxj_jJ8KvZJiPVt9DkplPDy_m9xNaPNrqg4in6gDW1PYQPRrzcVeRJ7zr_HjK5SKLLsUYgmRPNLs=w1366-h626-rw', currentPose:'https://lh4.googleusercontent.com/nVYZF3Il7kukpv-akm7AuNqp5ubNzyz0-hWkyIH_B0_CTTCXDM4TmCmNL0NgIizTgrFOTzW5OZ4itrIndvLF=w1366-h626-rw'}
+		,{name:'Marisa Espat', currentHeadshot:'https://lh5.googleusercontent.com/1-pDmKRpqAfa9Q4mccDTfohdSDryJsFZ2Xw22FVTB5hog8TtO6kz8ySoBQyiSFXY27b5H_VKZjs0lytHWfwa=w1366-h626-rw', currentPose:'https://lh6.googleusercontent.com/iyFHSQxfu5iEf4UumfHzaAkmzsGnQe_9VhkdVRLVTe3X7-KTxq9f5Qj2UvZ6HdraJeyHkMwYQaxpQrGNEW9n=w1366-h626-rw'}
+		,{name:'Alison Swiosh', currentHeadshot:'https://lh3.googleusercontent.com/mb8jW7yY4o3vBJWnSD8aEhRUp13mr-mxRbD3q3_ANvopn8HPM8vlFO779EdyOdDCVXGEvHGMVzqMaMjLv8rt=w1366-h626-rw', currentPose:'https://lh3.googleusercontent.com/JVODu9xcVPV6jEkUPE4gJ91ZWqeN6hHCvpaGynk85-CW5U_bg1Q3vWrUqKOMUIz4lgbbInDM4xpwSPCDnxcw=w1366-h626-rw'}
+		,{name:'Stephanie Freeman', currentHeadshot:'https://lh4.googleusercontent.com/ZcuJfT5zPFrPJCkaY7SCPwKwoSRrqrqkGC9ibtldT7LROwVUlizdtcT-FBUP7GY0QngdVWahaUqAdBPkBLjN=w1366-h626-rw', currentPose:'https://lh5.googleusercontent.com/U51sRYm5qdIgYEpU2EtTVJriURqACK5Lfzflq6Y0UijEi61fOZzKfksci6aw-fHLT3GJTbvBDUnoGJ-nnR29=w1366-h626-rw'}
+	]		
+}
+
+module.exports = {
+	dancers:dancers
+};
